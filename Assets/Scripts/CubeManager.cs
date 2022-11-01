e2f21b15c39f90f882cac568e03ff40ddb37eef1
Qvelent <qvelent@gmail.com>
2022-11-01 13:40:33 +0300

    setting canvas

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -1,10 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
-    [SerializeField] private float startTimeSpawn;
-    [SerializeField] private float distance;
-    [SerializeField] private float speed;
+    [SerializeField] private float _startTimeSpawn;
+    [SerializeField] private float _distance;
+    [SerializeField] private float _speed;
+    
+    [SerializeField] private TMP_InputField _distanceTMP;
+    [SerializeField] private TMP_InputField _timeTMP;
+    [SerializeField] private TMP_InputField _speedTMP;
     
     [SerializeField] private GameObject prefabCube;
     
@@ -12,7 +17,8 @@ public class CubeManager : MonoBehaviour
     
     void Start()
     {
-        _timeSpawn = startTimeSpawn;
+        ShowUI();
+        _timeSpawn = _startTimeSpawn;
     }
 
     void FixedUpdate()
@@ -23,7 +29,7 @@ public class CubeManager : MonoBehaviour
         }
         else
         {
-            _timeSpawn = startTimeSpawn;
+            _timeSpawn = _startTimeSpawn;
             SpawnCube();
         }
     }
@@ -31,8 +37,42 @@ public class CubeManager : MonoBehaviour
     private void SpawnCube()
     {
         GameObject spawn = Instantiate(prefabCube, Vector3.zero, Quaternion.identity);
-        Vector2 startPos = Vector2.up * distance;
+        Vector2 startPos = Vector2.up * _distance;
         Vector3 endPos = new Vector3(startPos.x, 0, startPos.y);
-        spawn.GetComponent<CubeMoving>().SetCubeMoving(speed, endPos);
+        spawn.GetComponent<CubeMoving>().SetCubeMoving(_speed, endPos);
+    }
+    
+    private void ShowUI()
+    {
+        _distanceTMP.text = "";
+        _timeTMP.text = "";
+        _speedTMP.text = "";
+        _distanceTMP.placeholder.GetComponent<TextMeshProUGUI>().text = "default: " + _distance;
+        _timeTMP.placeholder.GetComponent<TextMeshProUGUI>().text = "default: " + _startTimeSpawn;
+        _speedTMP.placeholder.GetComponent<TextMeshProUGUI>().text = "default: " + _speed;
+    }
+    
+    public void SetDistance(string distance)
+    {
+        if (!distance.Equals(""))
+        {
+            _distance = float.Parse(distance);
+        }
+    }    
+    
+    public void SetTime(string time)
+    {
+        if (!time.Equals(""))
+        {
+            _startTimeSpawn = float.Parse(time);
+        }
+    }
+    
+    public void SetSpeed(string speed)
+    {
+        if (!speed.Equals(""))
+        {
+            _speed = float.Parse(speed);
+        }
     }
 }
